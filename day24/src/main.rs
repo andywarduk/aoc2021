bdcf73355fa76e7a1f4f0ce7961d77bd7a109447
Andy <andy.ward.uk@gmail.com>
2021-12-25 01:31:58 +0000

    Day 24

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -96,32 +96,6 @@ fn solve(program: &[Instruction], max: bool) -> Vec<i64> {
     digits
 }
 
-// function solve(input, part) {
-
-//     const lines = input.split('\n');
-  
-//     const terms = [];
-//     for (let i = 0; i < lines.length; i += 18) {
-//       // terms [a, b, c] such that z'(a, b, c, w, z) = ~~(z / a) * (25 * +(w !== (z % 26) + b) + 1) + (w + c) * +(w !== (z % 26) + b)
-//       terms.push([4, 5, 15].map((j) => +lines[i + j].split(' ')[2]));
-//     }
-  
-//     const prevs = [];
-//     const digits = [];
-//     for (const [i, [a, b, c]] of Object.entries(terms)) {
-//       if (a === 1) {
-//         prevs.push([i, c]);
-//       } else {
-//         const [prevI, prevC] = prevs.pop();
-//         const complement = prevC + b;
-//         digits[prevI] =
-//           part === 1 ? Math.min(9, 9 - complement) : Math.max(1, 1 - complement);
-//         digits[i] = digits[prevI] + complement;
-//       }
-//     }
-//     console.log(digits.join(''));
-//   }
-
 #[derive(Debug)]
 enum Exception {
     DivByZero,
